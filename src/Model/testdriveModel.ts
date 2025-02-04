e00b10cbfc721f46e2f1877c7959c02db53f3927
Rajvirsinh Dabhi <rajvirsinhdabhi1@gmail.com>
2025-02-04 15:45:45 +0530

    testdrive model added

---
+++ src/Model/testdriveModel.ts
@@ -0,0 +1,37 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import { model, Types } from 'mongoose';
+// Define an interface for the User document
+interface ITestdrive{
+    carId: Types.ObjectId;  // Reference to Car
+    userId: Types.ObjectId; // Reference to User
+    date: Date;             // Test drive scheduled date
+    status: 'pending' | 'approved' | 'completed' | 'cancelled'; 
+}
+// Define the schema
+const testDriveSchema: Schema<ITestdrive> = new Schema(
+  {
+    carId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+      },
+      userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
+      date: {
+          type: Date,
+          required: true,
+        },
+      status: {
+        type: String,
+        enum: ['pending', 'approved', 'completed', 'cancelled'],
+        required: true,
+        default: 'pending'
+      },
+  },
+  { timestamps: true } // Automatically add createdAt and updatedAt fields
+);
+// Create the model
+const Testdrive = mongoose.model<ITestdrive>('Car', testDriveSchema);
+export default Testdrive;
